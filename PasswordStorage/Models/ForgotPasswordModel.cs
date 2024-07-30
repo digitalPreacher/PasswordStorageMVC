adda5ad9e39182f3fec6b74dfe80cec2a3b55f97
Николай Пронин <Proninkolian@yandex.ru>
2024-07-30 21:22:53 +0300

    Added html code in views

--- PasswordStorage/Models/ForgotPasswordModel.cs
+++ PasswordStorage/Models/ForgotPasswordModel.cs
@@ -4,12 +4,12 @@ namespace PasswordStorage.Models
 {
     public class ForgotPasswordModel
     {
-        [Required]
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
         [EmailAddress]
         [Display(Name = "Почта")]
         public string Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Поле обязательно для заполнения")]
         [Display(Name = "Секретное слово")]
         public string SecretWord { get; set; }
     }
